11f7bd00eea4a8fac79b873995bb40c665d20921
hifeche <117675004+hifeche@users.noreply.github.com>
2026-08-07 15:20:56 -0300

    Add files via upload

---
+++ Exercicios/2 - Entrada e Saída/Lista2 lógica.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int numero, antecessor, sucessor;
+	
+	printf("Escreva um numero inteiro: ");
+	
+	scanf("%d", &numero);
+	
+	antecessor = numero - 1;
+	sucessor = numero + 1;
+	
+	printf("Antecessor %d\n", antecessor);
+	printf("Sucessor %d\n", sucessor);
+	
+	
+	return 0;
+}
+
